2234bad7f9a648f522d0c922a9c7fe0703e8ade3
Jari Ojala <jari.ojala@iki.fi>
2015-04-03 23:59:31 +0300

    Addded support for angular-datepicker module (in pom.xml, not used). Improved competition selection. Implemented competition removal.

--- ui/src/main/resources/app/app.js
+++ ui/src/main/resources/app/app.js
@@ -12,106 +12,27 @@ app.controller('CompetitionSelectionController', function ($scope, $http) {
         alert(status);
     });
 
+    var selectedCompetitionId = null;
+
+    $scope.unixTimeToString = function(unixTime) {
+        var date = new Date(unixTime);
+        return date.toLocaleString();
+    }
+
+    $scope.onSelectionRequest = function(c, i, e) {
+        console.log("SELECT: Competition id:" + c.id + ". Index: " + i);
+    }
+
     $scope.onDeleteRequest = function(c, i, e) {
-        console.log("Delete requested for competition #" + c.id);
+        console.log("DELETE: Competition id: " + c.id + ". Index: " + i);
         console.log("    i=" + i);
+
+        $http.delete("rest/competition/" + c.id).success(function () {
+            $scope.competitions.splice(i, 1);
+        }).error(function (err) {
+            alert("Deleting competition failed: " + err.statusText);
+        });
     }
 
     $scope.orderProp = 'time';
 });
-
-function timeToString(time) {
-    var date = new Date(time);
-    return date.toLocaleDateString() + ' '
-            + date.toLocaleTimeString();
-};
-
-function Rest(request) {
-
-    this.create = function(url, data) {
-        return request.post(url, {
-            data : data,
-            handleAs : 'json',
-            headers : {
-                'Content-Type' : 'application/json',
-                'Accept' : 'application/json'
-            }
-        });
-    }
-
-    this.update = function(url, data) {
-        return request.put(url, {
-            data : data,
-            handleAs : 'json',
-            headers : {
-                'Content-Type' : 'application/json',
-                'Accept' : 'application/json'
-            }
-        });
-    }
-
-    this.destroy = function(url) {
-        return request.del(url, {
-            headers : {
-                'Content-Type' : 'application/json',
-                'Accept' : 'application/json'
-            }
-        });
-    }
-
-    this.get = function(url) {
-        return request(url, {
-            handleAs : 'json',
-            headers : {
-                'Content-Type' : 'application/json',
-                'Accept' : 'application/json'
-            }
-        });
-    }
-}
-
-function Competition(request, url, ctionId) {
-
-    this.get = function(onSuccess, onFailuere) {
-        request(url + '/competition/' + ctionId, {
-            'handleAs' : 'json',
-            'headers' : {
-                    'Content-Type' : 'application/json',
-                    'Accept' : 'application/json' 
-                }
-            }).then(
-                function(data) {
-                    onSuccess(data);
-                },
-                function(err) {
-                    onFailure(err);
-                }
-            );
-    }
-
-    this.create = function(ction, onSuccess, onFailure) {
-        Rest(request).create(url + '/competition/' + ctionId, c).then(
-                function(data) {
-                    alert('Created: ' + ction);
-                    onSuccess(ction);
-                },
-                function(err) {
-                    alert('Creation failed: ' + err);
-                    onFailure(err);
-                }
-            );
-    }
-
-    this.destroy = function(onSuccess, onFailure) {
-        Rest(request).destroy(url + '/competition/' + ctionId).then(
-                function(ok) {
-                    alert('Competition destroyed: ' + ctionId);
-                    onSuccess(ok);
-                },
-                function(err) {
-                    alert('Destroying competition failed: ' + err);
-                    onFailure(err);
-                }
-            );
-    }
-}
