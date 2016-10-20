244b81a1003f8e294f53212b51b2a77856ce23ac
Jari Ojala <jari.ojala@iki.fi>
2016-10-21 00:02:47 +0300

    #2 Refactored CompetitionMainController and CompetitionListController into a dedicated files in app/components.

--- ui/src/main/webapp/app/app.routes.js
+++ ui/src/main/webapp/app/app.routes.js
@@ -5,16 +5,16 @@
 (function() {
     'use strict';
     
-    angular.module('app.routes', [ 'ngRoute' ]).config(['$routeProvider', config]);
+    angular.module('app.routes', [ 'ngRoute', 'app.competition' ]).config(['$routeProvider', config]);
     
     function config($routeProvider) {
         $routeProvider
            .when('/competition-list', {
-                templateUrl: '/app/components/competition-list/competition-list-tmpl.html',
+                templateUrl: '/app/components/competition-list/competition-list.tmpl.html',
                 controller: 'CompetitionListController'
             })
            .when('/competition/:competitionId', {
-                templateUrl: '/app/components/competition/competition-main-tmpl.html',
+                templateUrl: '/app/components/competition-main/competition-main.tmpl.html',
                 controller: 'CompetitionMainController'
             })
            .otherwise({
---
+++ ui/src/main/webapp/app/components/competition-list/competition-list.js
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2015-2016 Jari Ojala (jari.ojala@iki.fi).
+ */
+
+(function() {
+'use strict';
+
+var app = angular.module('app.competition',
+    [ 'utils', 'ngRoute', 'ui.bootstrap', 'ngWebsocket' ]);
+
+app.controller('CompetitionListController',
+    function ($scope, $http, Uuid, Rcnp) {
+
+        $scope.current = null;
+        $scope.competitions = [];
+        $scope.competitionDetailsShow = false;
+        $scope.eventImportDialogVisible = false;
+        
+        $scope.showEventImportDialog = function() {
+            $scope.eventImportDialogVisible = true;
+        };
+        
+        $scope.hideEventImportDialog = function() {
+            $scope.eventImportDialogVisible = false;
+        };
+        
+        $scope.openCompetitionDetails = function (c, i) {
+            $scope.competitionDetailsShow = true;
+        };
+        
+        $scope.hideCompetitionDetails = function() {
+            $scope.current = null;
+            $scope.competitionDetailsShow = false;
+        };
+        
+        Rcnp.register(function(c) {
+                $scope.$apply(function() {
+                    $scope.competitions.push(c);
+                });
+            },
+            'CREATED', 'org.gemini.results.model.Event');
+
+        Rcnp.register(function(c) {
+            $scope.$apply(function() {
+                for (var i = 0; i < $scope.competitions.length; i++) {
+                    if ($scope.competitions[i].id == c.id) {
+                        $scope.competitions[i] = c;
+                        break;
+                    }
+                }
+            });
+        }, 'UPDATED', 'org.gemini.results.model.Event');
+
+        Rcnp.register(function(c) {
+            $scope.$apply(function() {
+                for (var i = 0; i < $scope.competitions.length; i++) {
+                    if ($scope.competitions[i].id === c.id) {
+                        $scope.competitions.splice(i, 1);
+                        break;
+                    }
+                }
+            });
+        }, 'REMOVED', 'org.gemini.results.model.Event');
+        
+        $http.get("rest/events").success(function (data) {
+            for (var i = 0; i < data.length; ++i)
+                data[i].timeObject = new Date(data[i].time);
+
+            $scope.competitions = data;
+        }).error(function (err, status) {
+            alert('Retrieving events failed: \n' + 'err: ' + err + '\n' +
+                    'status: ' + status);
+        });
+
+        $scope.onSelect = function(c) {
+            $scope.current = {
+                id: c.id,
+                time: new Date(c.time),
+                name: c.name,
+                organizer: c.organizer
+            };
+        };
+
+        $scope.onCreate = function(c) {
+            c.id = Uuid.randomUUID();
+            $http.post("rest/events/" + c.id, c)
+                .success(function() { $scope.current = {}; })
+                .error(function(err, status) {
+                    alert('Adding event failed: \n' + 'err: '  + err + '\n' +
+                        'status: ' + status + '\n' + 'event: ' +
+                        angular.toJson(c, true));
+                    });
+            };
+
+        $scope.onSave = function(c) {
+            delete c.timeObject;
+            $http.put("rest/events/" + c.id, c)
+                .success(function() { $scope.current = {}; })
+                .error(function (err, status) {
+                    alert('Updating event failed: \n' + 'err: ' + err + '\n' +
+                        'status: ' + status + '\n' + 'event: ' +
+                        angular.toJson(c, true));
+                    });
+            };
+
+        $scope.onDestroy = function(c, i) {
+            $http.delete("rest/events/" + c.id)
+                .error(function (err, status) {
+                    alert('Deleting event failed: \n' + 'err: ' + err + '\n' +
+                        'status: ' + status + '\n' + 'event: ' +
+                        angular.toJson(c, true));
+                });
+        };
+
+        $scope.onDownload = function(c) {
+            $http.get("rest/events/export/" + c.id);
+        };
+
+        $scope.sortCriteria = 'time';
+    }
+);
+})();
