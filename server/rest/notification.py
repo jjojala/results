3d91b19caeeca6176f4390c7c320f185b4fa2a4c
Jari Ojala <jari.ojala@patria.fi>
2019-02-15 17:17:12 +0200

    Added support for UUID -based nodeId in RCNP.

--- server/rest/notification.py
+++ server/rest/notification.py
@@ -1,4 +1,5 @@
 from flask_socketio import SocketIO, Namespace
+import uuid
 
 CREATED = 'CREATED'
 UPDATED = 'UPDATED'
@@ -10,11 +11,11 @@ class Notifications(Namespace):
 	def __init__(self, namespace, socketio):
 		super(Notifications, self).__init__(namespace)
 		self._socketio = socketio
-		self._nodeId = 123
+		self._nodeId = str(uuid.uuid4())
 
 	def submit(self, event, entityType, entityId, data):
 		notification = {
-			'event': event + ' ' + entityType + ' ' + entityId + ' ' + str(self._nodeId),
+			'event': event + ' ' + entityType + ' ' + entityId + ' ' + self._nodeId,
 			'data': data
 		}
 		self._socketio.emit('notification', notification, namespace='/api/notifications')
