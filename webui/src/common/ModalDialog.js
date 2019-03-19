1a73c24dc4ea34a7402540f08d14a879207b1cb0
Jari Ojala <jari.ojala@iki.fi>
2019-03-19 22:48:42 +0200

    Added preliminary "dialog" for typing new events.

---
+++ webui/src/common/ModalDialog.js
@@ -0,0 +1,42 @@
+import './ModalDialog.css';
+import React, { Component } from 'react';
+
+export class ModalDialog extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const backdropStyle = {
+            position: 'fixed',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            backgroundColor: 'rgba(0,0,0,0.3)',
+            padding: 50
+        };
+      
+        // The modal "window"
+        const modalStyle = {
+            backgroundColor: '#fff',
+            borderRadius: 5,
+            maxWidth: 500,
+            minHeight: 300,
+            margin: '0 auto',
+            padding: 30
+        };
+      
+        if (!this.props.show) {
+            return null;
+        }
+
+        return (
+            <div className="backdrop" style={{backdropStyle}}>
+                <div className="modal" style={{modalStyle}}>
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+}
--- webui/src/event/EventListView.js
+++ webui/src/event/EventListView.js
@@ -1,24 +1,75 @@
-import './EventListView.css'
-import NewImg from 'images/svg/003-more.svg'
-import DelImg from 'images/svg/006-rounded-delete-button-with-minus.svg'
-import NextImg from 'images/svg/016-next-page.svg'
+import './EventListView.css';
+import NewImg from 'images/svg/003-more.svg';
+import DelImg from 'images/svg/006-rounded-delete-button-with-minus.svg';
+import NextImg from 'images/svg/016-next-page.svg';
+import OkImg from 'images/svg/004-check-mark-button.svg';
+import CancelImg from 'images/svg/005-cancel.svg';
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { randomUUID } from "../util";
 import { View } from "../common/View.js";
+import { ModalDialog } from '../common/ModalDialog.js';
+import { throws } from 'assert';
+
+class NewEventDialog extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <ModalDialog show={this.props.show}>
+                <h1>Uusi tapahtuma</h1>
+                <button onClick={() => this.props.onClose()}>
+                    <CancelImg height={32} width={32}/>
+                </button>
+                <button onClick={() => this.props.onCreate()}>
+                    <OkImg height={32} width={32}/>
+                </button>
+            </ModalDialog>
+        )
+    }
+}
 
 class EventListContent extends Component {
     constructor(props) {
         super(props);
-        this.state = props.states();
+        this.state = {
+            states: props.states(),
+            isNewEventDialogOpen: false
+        }
     }
     
     componentDidMount() {
         var setState = this.setState.bind(this);
+        this.openNewEventDialog.bind(this);
+        this.closeNewEventDialog.bind(this);
+        this.createNewEvent.bind(this);
+
         this.props.states.map(function(state) {
-            setState(state);
+            setState({ states: state });
         });
         this.props.actions.loadEvents();
+    }
+
+    openNewEventDialog() {
+        this.setState({ isNewEventDialogOpen: true });
+    }
+
+    closeNewEventDialog() {
+        console.log('closeNewEventDialog()')
+        // TODO: Reset dialog content...
+        this.setState({ isNewEventDialogOpen: false });
+    }
+
+    createNewEvent(e) {
+        console.log('createNewEvent()')
+        const eid = randomUUID();
+        this.props.actions.createEvent({
+            id: eid, date: "2019-02-05T10:42:14.000+03:00",
+            name: "Name (id: " + eid + ")"});
+        
+        this.setState({ isNewEventDialogOpen: false });
     }
 
     render() {
@@ -26,7 +77,7 @@ class EventListContent extends Component {
         var actions = this.props.actions;
         const id = randomUUID();
         
-        const items = state.events.map(e => {
+        const items = state.states.events.map(e => {
             return (
                 <div key={e.id} style={{display: "flex", flexDirection: "row"}}>
                     <span style={{width: "200px"}}>
@@ -50,9 +101,14 @@ class EventListContent extends Component {
                 </div>
             );
         });
-        
-        return(
+
+        return (
             <div style={{display: "flex", flexDirection: "column", height: "100%"}}>
+                <NewEventDialog show={this.state.isNewEventDialogOpen}
+                        onClose={() => this.closeNewEventDialog()}
+                        onCreate={(e) => this.createNewEvent(e)}>
+                </NewEventDialog>
+
                 <div style={{display: "flex", flexDirection: "row", backgroundColor: "lightgray", marginBottom: "16px", fontWeight: "bold"}}>
                     <span style={{width: "200px"}}>
                         Päivä ja aika
@@ -61,9 +117,7 @@ class EventListContent extends Component {
                         Tapahtuman nimi
                     </span>
                     <span>
-                        <button onClick={() => actions.createEvent({ 
-                            id: id, date: "2019-02-05T10:42:14.000+03:00",
-                            name: "Name (id=" + id + ")"})}>
+                        <button onClick={() => this.openNewEventDialog()}>
                             <NewImg height={32} width={32}/>
                         </button>
                     </span>
